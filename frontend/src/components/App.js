00dd3bfc939ec52b20a5790e05e34288dd6a252e
Aditya Unnikrishnan <adiunni@hotmail.com>
2023-08-06 10:43:17 +0530

    Config update

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,35 +1,43 @@
-import React, {useState,useEffect} from 'react';
-import {Link} from 'react-router-dom'
-import logo from '../assets/logo.png';
-import {API_BASE_URL} from '../config'
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { API_BASE_URL } from "../config";
 
 function App() {
-  const [walletInfo,setWalletInfo] = useState({})
+  const [walletInfo, setWalletInfo] = useState({});
+  const [apiURL] = useState(localStorage.getItem("NetURL") || API_BASE_URL);
 
   useEffect(() => {
-    fetch(`${API_BASE_URL}/wallet/info`)
-      .then(response=>response.json())
-      .then(json => setWalletInfo(json));
-  },[])
+    fetch(`${apiURL}/wallet/info`)
+      .then((response) => response.json())
+      .then((json) => setWalletInfo(json));
+  }, [apiURL]);
 
-  const {address,balance} = walletInfo;
+  const { address, balance } = walletInfo;
 
-  return(
-    <div className='App'>
-      <img className='logo' src={logo} alt='App Logo'/>
+  return (
+    <div className="App">
+      <br />
+      <img
+        className="logo"
+        src={logo}
+        style={{
+          width: "150px",
+          height: "150px",
+        }}
+        alt="App Logo"
+      />
       <h3>Welcome to ProtoCrit</h3>
-      <br/>
-      <Link to="/blockchain" >Blockchain</Link>
+      <br />
+      <Link to="/blockchain">Blockchain</Link>
       <Link to="/conduct-transaction">Conduct transaction</Link>
       <Link to="/transaction-pool">Transaction pool</Link>
-      <div className='WalletInfo'>
+      <div className="WalletInfo">
         <div>Address: {address}</div>
         <div>Balance: {balance}</div>
       </div>
     </div>
-  )
+  );
 }
-
 
 export default App;
---
+++ frontend/src/components/NetURL.js
@@ -0,0 +1,30 @@
+import { API_BASE_URL } from "../config";
+import { useState } from "react";
+
+export default function NetURL() {
+  const [baseURL, setBaseURL] = useState(
+    localStorage.getItem("NetURL") || API_BASE_URL
+  );
+
+  return (
+    <div className="NetURL">
+      <div>
+        <input
+          type="text"
+          placeholder="NetURL"
+          value={baseURL}
+          onChange={(e) => setBaseURL(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            // Add to localStorage
+            localStorage.setItem("NetURL", baseURL);
+          }}
+        >
+          Fetch
+        </button>
+      </div>
+      <br />
+    </div>
+  );
+}
